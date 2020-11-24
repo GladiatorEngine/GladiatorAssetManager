19e81310ecba58ec6d850219f500e2b04eac8670
Pavel Kosilo <kodosgames@gmail.com>
2020-11-24 11:41:10 +0300

    Add AssetType in AssetPack entity

--- Sources/GladiatorAssetManager/GladiatorAssetManager.swift
+++ Sources/GladiatorAssetManager/GladiatorAssetManager.swift
@@ -77,7 +77,7 @@ public struct GladiatorAssetManager {
         for asset in assets {
             let aData = asset.assetData()
             var length: Int = aData.endIndex
-            data = data + Data(bytes: &length, count: MemoryLayout<Int>.size) + aData
+            data = Data([asset.assetType().rawValue]) + data + Data(bytes: &length, count: MemoryLayout<Int>.size) + aData
         }
         
         return data
