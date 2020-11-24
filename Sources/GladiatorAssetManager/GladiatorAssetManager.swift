a3a8df6322a7e413965963333a3374a36be325be
Pavel Kosilo <kodosgames@gmail.com>
2020-11-24 10:33:11 +0300

    Add asset data verification with hash

--- Sources/GladiatorAssetManager/GladiatorAssetManager.swift
+++ Sources/GladiatorAssetManager/GladiatorAssetManager.swift
@@ -24,8 +24,15 @@ public struct GladiatorAssetManager {
             let assetTypeByte: UInt8 = fullAsset.subdata(in: 0..<2).withUnsafeBytes {$0.pointee}
             
             guard let assetType = AssetType(rawValue: assetTypeByte) else {throw AssetLoadErrors.failedToParseType(byte: assetTypeByte)}
+            let data = fullAsset.subdata(in: 2..<fullAsset.endIndex-128)
             
-            return (assetType, fullAsset.subdata(in: 2..<fullAsset.endIndex-128))
+            // Verify data with hash
+            let hash = fullAsset.subdata(in: fullAsset.endIndex-128..<fullAsset.endIndex)
+            if Data() + SHA512.hash(data: data) != hash {
+                fatalError("Asset hash is not valid!")
+            }
+            
+            return (assetType, data)
         } catch let e {
             fatalError("Cannot load asset: \(e)")
         }
