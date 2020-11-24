4d67688d4fd8fca57b5f4cfbf06cbf0621ce00bc
Pavel Kosilo <kodosgames@gmail.com>
2020-11-24 21:57:07 +0300

    Edit Model Asset

--- Sources/GladiatorAssetManager/Assets/Model.swift
+++ Sources/GladiatorAssetManager/Assets/Model.swift
@@ -10,16 +10,24 @@ import Foundation
 public struct Model: Asset {
     public init(sourceData: Data) {
         let amountOfVertices: Int = sourceData.subdata(in: 0..<MemoryLayout<Int>.size).withUnsafeBytes {$0.pointee}
-        var vertices = [Float]()
+        var vertices = [[Float]]()
         for i in 0..<amountOfVertices {
-            let pos = MemoryLayout<Int>.size + MemoryLayout<Float>.size * i
-            vertices.append(sourceData.subdata(in: pos..<pos+MemoryLayout<Float>.size).withUnsafeBytes {$0.pointee})
+            for j in 0..<4 {
+                let pos = MemoryLayout<Int>.size + MemoryLayout<Float>.size * ((i+1)*(j+1)-1)
+                vertices.append(sourceData.subdata(in: pos..<pos+MemoryLayout<Float>.size).withUnsafeBytes {$0.pointee})
+            }
         }
+        self.vertices = vertices
+    }
+    
+    public init(vertices: [[Float]]) {
         self.vertices = vertices
     }
     
     public func assetData() -> Data {
         var data = Data()
+        
+        data = data + withUnsafeBytes(of: self.vertices.count) { Data($0) }
         
         for vertex in self.vertices {
             data = data + withUnsafeBytes(of: vertex) { Data($0) }
@@ -32,6 +40,6 @@ public struct Model: Asset {
         return .model
     }
     
-    public private(set) var vertices: [Float]
+    public private(set) var vertices: [[Float]]
 }
 
